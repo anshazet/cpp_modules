c2b6e1e1f10395bcefbf5b7efe3a7d93bb5442eb
Anastasia Shalaginova Zetzner <anastasia@MacBook-Pro-de-Anastasia.local>
2024-01-04 11:05:49 +0100

    module 06

--- M_06/ex00/ScalarConverter.cpp
+++ M_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,98 @@
 /*   By: ashalagi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:35:42 by ashalagi          #+#    #+#             */
-/*   Updated: 2024/01/03 14:35:43 by ashalagi         ###   ########.fr       */
+/*   Updated: 2024/01/04 09:30:45 by ashalagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+#include <cmath>
+
+ScalarConverter::ScalarConverter() {}
+
+void ScalarConverter::convert(const std::string &literal)
+{
+	// Detect and convert the type
+	if (isChar(literal))
+	{
+		convertChar(literal);
+	}
+	else if (isInt(literal))
+	{
+		convertInt(literal);
+	}
+	else if (isFloat(literal))
+	{
+		convertFloat(literal);
+	}
+	else if (isDouble(literal))
+	{
+		convertDouble(literal);
+	}
+	else
+	{
+		std::cout << "Unknown type" << std::endl;
+	}
+}
+
+void ScalarConverter::convertChar(const std::string &literal)
+{
+	if (literal.length() != 3 || literal[0] != '\'' || literal[2] != '\'')
+	{
+		throw std::runtime_error("Invalid character literal format.");
+	}
+	char c = literal[1];
+
+	// Convert to char
+	std::cout << "char: ";
+	if (isPrintable(c))
+	{
+		std::cout << "'" << c << "'" << std::endl;
+	}
+	else
+	{
+		std::cout << "Non displayable" << std::endl;
+	}
+
+	// Convert to int
+	std::cout << "int: " << static_cast<int>(c) << std::endl;
+
+	// Convert to float
+	std::cout << "float: " << static_cast<float>(c) << "f" << std::endl;
+
+	// Convert to double
+	std::cout << "double: " << static_cast<double>(c) << std::endl;
+}
+
+void ScalarConverter::convertInt(const std::string &literal)
+{
+    // Implementation for int conversion
+    // ...
+}
+
+void ScalarConverter::convertFloat(const std::string &literal)
+{
+    // Implementation for float conversion
+    // ...
+}
+
+void ScalarConverter::convertDouble(const std::string &literal)
+{
+    // Implementation for double conversion
+    // ...
+}
+
+
+bool ScalarConverter::isPrintable(char c)
+{
+    return std::isprint(static_cast<unsigned char>(c));
+}
+
+// Define isChar, isInt, isFloat, isDouble, isPrintable, etc.
+// ...
+
+// Additional helper methods implementation
+// ...
--- M_06/ex00/ScalarConverter.hpp
+++ M_06/ex00/ScalarConverter.hpp
@@ -6,7 +6,39 @@
 /*   By: ashalagi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:35:46 by ashalagi          #+#    #+#             */
-/*   Updated: 2024/01/03 14:35:46 by ashalagi         ###   ########.fr       */
+/*   Updated: 2024/01/04 08:15:18 by ashalagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string>
+
+class ScalarConverter
+{
+public:
+    // The convert method is static, as it does not depend on any instance of ScalarConverter.
+    static void convert(const std::string &literal);
+
+private:
+    // Private constructor and assignment operator to prevent instantiation.
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter &);
+    ScalarConverter &operator=(const ScalarConverter &);
+
+    // Helper methods for conversion and output.
+    static void convertChar(const std::string &literal);
+    static void convertInt(const std::string &literal);
+    static void convertFloat(const std::string &literal);
+    static void convertDouble(const std::string &literal);
+
+    // Additional helper methods.
+    static bool isChar(const std::string &literal);
+    static bool isInt(const std::string &literal);
+    static bool isFloat(const std::string &literal);
+    static bool isDouble(const std::string &literal);
+    static bool isPrintable(char c);
+};
+
+#endif // SCALARCONVERTER_HPP
